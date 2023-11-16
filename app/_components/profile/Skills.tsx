4221e76bea6a5cab3ab347b517ae22526b1485a8
Guillaume Delorme <guillaume.delorme.fr@gmail.com>
2023-11-16 23:26:54 +0100

    ✨ Adding skills

---
+++ app/_components/profile/Skills.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Card } from "../common/Card";
+import { Title } from "../common/Title";
+
+const Skill = ({ children }: { children: React.ReactNode }) => (
+  <div className="rounded-lg bg-sky-100 px-2 py-1 text-sm font-medium text-sky-400">
+    {children}
+  </div>
+);
+
+export const Skills = () => (
+  <Card>
+    <div className="flex w-full flex-col gap-3 ">
+      <Title level="h2">Skills</Title>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">
+        Languages
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>TypeScript</Skill>
+        <Skill>Rust</Skill>
+        <Skill>Java</Skill>
+      </div>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">
+        Frontend
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>React</Skill>
+        <Skill>Next.js</Skill>
+        <Skill>Vue</Skill>
+        <Skill>Tauri</Skill>
+        <Skill>Three.js</Skill>
+        <Skill>TailwindCSS</Skill>
+      </div>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">
+        Backend
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>Serverless</Skill>
+        <Skill>Nest.js</Skill>
+        <Skill>Temporal.io</Skill>
+      </div>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">
+        Base de données
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>PostgreSQL</Skill>
+        <Skill>MariaDB</Skill>
+      </div>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">
+        DevOps / Infrastructure
+      </div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>AWS</Skill>
+        <Skill>Scaleway</Skill>
+        <Skill>Docker</Skill>
+        <Skill>GitHub actions</Skill>
+        <Skill>Architecture</Skill>
+        <Skill>Infrastructure as Code</Skill>
+      </div>
+
+      <div className="flex gap-4 text-sm font-medium text-gray-500">Autre</div>
+      <div className="flex flex-wrap gap-2">
+        <Skill>Team Lead</Skill>
+        <Skill>Français</Skill>
+        <Skill>Anglais</Skill>
+      </div>
+    </div>
+  </Card>
+);
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Education } from "./_components/education/Education";
 import { Experience } from "./_components/experience/Experience";
+import { Skills } from "./_components/profile/Skills";
 
 export default function Home() {
   return (
-    <main className="flex max-w-screen-lg items-center justify-center p-10">
+    <main className="flex max-w-screen-lg items-start justify-center gap-6 p-10">
+      <div className="flex flex-col gap-6">
+        <Skills />
+      </div>
       <div className="flex flex-col gap-6">
         <Experience />
         <Education />
